32c751c04ab4a1cc6b41d0e6d11a3058ffb39ca5
Santigomezg22 <143724881+Santigomezg22@users.noreply.github.com>
2023-09-05 09:36:00 -0500

    Add files via upload

---
+++ Tareas.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package variables;
+
+/**
+ *
+ * @author labcca
+ */
+public class Tareas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int tareas; 
+        String tarea1 = "Calculo";  
+        String tarea2 = "Fundamentos";
+
+        System.out.println("La primera tarea a realizar " + tarea1 + " la segunda tarea a realizar es " + tarea2); 
+    }
+    
+}
