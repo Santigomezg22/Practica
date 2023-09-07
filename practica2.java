c97e1b05cbddea2b86a8a2128c8f23c73021e854
Santigomezg22 <143724881+Santigomezg22@users.noreply.github.com>
2023-09-07 08:44:09 -0600

    Add files via upload

---
+++ practica2.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package operaciones;
+
+/**
+ *
+ * @author Usuario
+ */
+public class practica2 {
+    
+    public static void main(String[] args) {
+        
+        int base = 15;
+        int altura = 10;
+        
+        int areaRectangulo = (base * altura)/2;
+        
+        System.out.println("El area del rectangulo es: " + areaRectangulo);
+        System.out.println("Mi nombre es Santiago Gomez Gracian");
+        
+        
+        
+        
+    }
+    
+}
